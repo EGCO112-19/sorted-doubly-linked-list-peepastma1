08ddb8892c7920ea130195f1a6bf34ba5205b98c
peepastma1 <natthabodi.boc@student.mahidol.edu>
2021-02-19 06:52:08 +0000

    change linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -3,6 +3,10 @@ struct Node {
    int data; // each listNode contains a character 
    struct Node *nextPtr; // pointer to next node
 }; // end structure listNode                        
+
+typedef struct Node LLnode; // synonym for struct listNode
+typedef LLnode *LLPtr; // synonym for ListNode*
+
 // prototypes
 
 int deletes( LLPtr *sPtr, int value );
@@ -10,9 +14,6 @@ int isEmpty( LLPtr sPtr );
 void insert( LLPtr *sPtr, int value );
 void printList( LLPtr currentPtr );
 void instructions( void );
-
-typedef struct Node LLnode; // synonym for struct listNode
-typedef LLnode *LLPtr; // synonym for ListNode*
 
 
 // display program instructions to user
